f6f7c286cf61907121dc57b661802edddd953717
BENETHNGOSWE <princebeneth1701@gmail.com>
2024-02-09 12:49:17 +0300

    career updates

--- resources/views/Careerers/index.blade.php
+++ resources/views/Careerers/index.blade.php
@@ -1,14 +1,33 @@
-<div class="container">
-    <h1>Career Opportunities</h1>
-    <form action="{{ route('careerer.program') }}" method="get">
-        <div class="form-group">
-            <label for="program">Search Program:</label>
-            <input type="search" id="program" name="program" class="form-control" value="{{ isset($program) ? $program : ''}}">
+@extends('frontend.main')
+@section('content')
+<section class="hero-section d-flex justify-content-center align-items-center" id="section_1">
+    <div class="container">
+        <div class="row">
+    <div class="col-lg-8 col-12 mx-auto">
+        <h1 class="text-white text-center">Career Opportunitiet</h1>
+    <form method="get" class="custom-form mt-4 pt-2 mb-lg-0 mb-5" role="search">
+        <div class="input-group input-group-lg">
+            <span class="input-group-text bi-search" id="basic-addon1">
+
+            </span>
+            @if(session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+            <form action="{{ route('careerer.program') }}" method="get">
+                
+                    <input type="search" id="program" name="program" class="form-control" value="{{ isset($program) ? $program : ''}}">
+               
+                <button type="submit" class="btn btn-primary">Search</button>
+            </form>
         </div>
-        <button type="submit" class="btn btn-primary">Search</button>
     </form>
+    </div>
     <br>
 
+        </div>
+    </div>
+</section>
+<div class="container">
     <table class="table">
         <thead>
             <tr>
@@ -30,3 +49,4 @@
         </tbody>
     </table>
 </div>
+@endsection
